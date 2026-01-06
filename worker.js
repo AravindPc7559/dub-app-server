6c0d795eba77e1ac74207edede1da1c69c0a3805
codewitharavind <codewitharavind7559@gmail.com>
2026-01-06 19:54:18 +0530

    docker production fix

--- worker.js
+++ worker.js
@@ -4,5 +4,14 @@ require('dotenv').config();
 const connectDB = require('./config/database');
 const { startWorker } = require('./services/worker.service');
 
-connectDB();
-startWorker();
+(async () => {
+    try {
+        await connectDB();
+        console.log('MongoDB connected in worker');
+        await startWorker();
+        console.log('Worker started');
+    } catch (error) {
+        console.error('Worker error:', error);
+        process.exit(1);
+    }
+})()
